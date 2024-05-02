afe91b45333184ce52028a3f06293871f7caf54e
raoasidg <raoasidg@users.noreply.github.com>
2024-05-02 15:44:05 -0400

    Release v2.1.1 Combine the initialization and the definition of the metamethods for the localization table.

--- Localization/Init.lua
+++ Localization/Init.lua
@@ -1,9 +1,6 @@
 local _, namespace = ...
-namespace.localizations = {}
 
 -- Local references
-local localizations = namespace.localizations
-
 local setmetatable, tostring =
       setmetatable, tostring
 
@@ -12,12 +9,12 @@ local API_GetLocale = GetLocale
 local client_locale = API_GetLocale()
 if client_locale == "enGB" then client_locale = "enUS" end
 
-setmetatable(localizations, {
-    __call = function(_, locale)
-        localizations[locale] = {}
-        return localizations[locale]
+namespace.localizations = setmetatable({}, {
+    __call = function(self, locale)
+        self[locale] = {}
+        return self[locale]
     end,
-    __index = function(_, key)
-        return localizations[client_locale] and localizations[client_locale][key] or localizations.enUS[key] or tostring(key)
+    __index = function(self, key)
+        return self[client_locale] and self[client_locale][key] or self.enUS[key] or tostring(key)
     end,
 })
