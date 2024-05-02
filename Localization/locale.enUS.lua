cef40c210f1c605849f8379a55b7f70ad5c6a256
raoasidg <raoasidg@users.noreply.github.com>
2024-05-02 12:28:02 -0400

    v2.1.0 commit

---
+++ Localization/locale.enUS.lua
@@ -0,0 +1,23 @@
+local _, namespace = ...
+local localizations = namespace.localizations("enUS")
+
+localizations.CHALLENGE_DUNGEON_AND_ABOVE = "and above"
+
+localizations.CREST_NAME_VETERAN = "Whelpling"
+localizations.CREST_NAME_CHAMPION = "Drake"
+localizations.CREST_NAME_HERO = "Wyrm"
+localizations.CREST_NAME_MYTH = "Aspect"
+
+localizations.DEFAULT_CURRENCY_SOURCE = "Various Activities"
+
+localizations.HEADER_CREST_CURRENT = "Current"
+localizations.HEADER_CREST_NEXT = "Next"
+localizations.HEADER_UPGRADE_TRACK = "Upgrade Track"
+
+localizations.UPGRADE_TRACK_NAME_EXPLORER = "Explorer"
+localizations.UPGRADE_TRACK_NAME_ADVENTURER = "Adventurer"
+localizations.UPGRADE_TRACK_NAME_VETERAN = "Veteran"
+localizations.UPGRADE_TRACK_NAME_CHAMPION = "Champion"
+localizations.UPGRADE_TRACK_NAME_HERO = "Hero"
+localizations.UPGRADE_TRACK_NAME_MYTH = "Myth"
+localizations.UPGRADE_TRACK_NAME_EX1 = "Awakened"
