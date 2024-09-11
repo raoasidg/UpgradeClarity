36f77003c6c00b5f2658cd8c0999efc4347e798f
raoasidg <82951927+raoasidg@users.noreply.github.com>
2024-09-11 00:25:25 -0400

    Release v2.3 (#11)
    Add delves to crest source information.
    
    Co-authored-by: raoasidg <raoasidg@users.noreply.github.com>

--- Localization/locale.enUS.lua
+++ Localization/locale.enUS.lua
@@ -1,8 +1,6 @@
 local _, namespace = ...
 local localizations = namespace.localizations("enUS")
 
-localizations.CHALLENGE_DUNGEON_AND_ABOVE = "and above"
-
 localizations.CREST_NAME_VETERAN = "Weathered"
 localizations.CREST_NAME_CHAMPION = "Carved"
 localizations.CREST_NAME_HERO = "Runed"
@@ -14,6 +12,8 @@ localizations.HEADER_CREST_CURRENT = "Current"
 localizations.HEADER_CREST_NEXT = "Next"
 localizations.HEADER_UPGRADE_CRESTS = "Upgrade Track Materials"
 localizations.HEADER_UPGRADE_TRACK = "Track"
+
+localizations.SOURCE_AND_ABOVE = "and above"
 
 localizations.UPGRADE_TRACK_NAME_EXPLORER = "Explorer"
 localizations.UPGRADE_TRACK_NAME_ADVENTURER = "Adventurer"
