7075a9aa03b820c04af61c48008175290f81343c
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-10-08 18:50:24 +0500

    Update locale.ruRU.lua

--- Localization/locale.ruRU.lua
+++ Localization/locale.ruRU.lua
@@ -1,6 +1,20 @@
 local _, namespace = ...
 local localizations = namespace.localizations("ruRU")
 
+localizations.CREST_NAME_VETERAN = "Истертый"
+localizations.CREST_NAME_CHAMPION = "Резной"
+localizations.CREST_NAME_HERO = "Рунический"
+localizations.CREST_NAME_MYTH = "Позолоченный"
+
+localizations.DEFAULT_CURRENCY_SOURCE = "Некоторые активности"
+
+localizations.HEADER_CREST_CURRENT = "Текущий"
+localizations.HEADER_CREST_NEXT = "Следующий"
+localizations.HEADER_UPGRADE_CRESTS = "Отслеживание улучшения предметов"
+localizations.HEADER_UPGRADE_TRACK = "Отслеживание"
+
+localizations.SOURCE_AND_ABOVE = "и выше"
+
 localizations.UPGRADE_TRACK_NAME_EXPLORER = "Исследователь"
 localizations.UPGRADE_TRACK_NAME_ADVENTURER = "Искатель приключений"
 localizations.UPGRADE_TRACK_NAME_VETERAN = "Ветеран"
